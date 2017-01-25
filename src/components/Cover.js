e118779c24626b1392fa641fb314b8b047877232
Trevor Sargent <tbird1710@gmail.com>
2017-01-24 22:24:08 -0800

    more style

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -26,9 +26,17 @@ render() {
 							</div>
 						</div>
 						<div className="info">
-							<p id="info">
-								<a href="https://www.google.com/maps/dir//1010+River+St,+Santa+Cruz,+CA+95060/@36.9867113,-122.0317947,17z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x808e4037ab7bf8ab:0xc27252656c7dbec7!2m2!1d-122.029606!2d36.986707">1010 River Street, Santa Cruz, CA 95060</a> | <a href="tel:+18318547458">831.854.7458</a> | Opening Soon...
-							</p>
+							<div id="info">
+								<div>
+									<a href="https://www.google.com/maps/dir//1010+River+St,+Santa+Cruz,+CA+95060/@36.9867113,-122.0317947,17z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x808e4037ab7bf8ab:0xc27252656c7dbec7!2m2!1d-122.029606!2d36.986707">1010 River Street, Santa Cruz, CA 95060</a>
+								</div>
+								<div>
+								<a href="tel:+18318547458">831.854.7458</a>
+								</div>
+								<div>
+								<a>Opening Soon...</a>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
