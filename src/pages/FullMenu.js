967829fec065696b52bfe951d0cccc1c82c22cba
Trevor Sargent <tbird1710@gmail.com>
2017-02-06 18:20:14 -0800

    menu stuff

--- src/pages/FullMenu.js
+++ src/pages/FullMenu.js
@@ -4,11 +4,21 @@ import React, {
 import * as Actions from '../actions/Actions'
 import Rebase from 're-base'
 import VarStore from '../stores/VarStore'
+import UUID from 'uuid'
+import data from '../menu.json'
 
 export default class FullMenu extends Component {
 
 	constructor(){
 		super();
+
+		this.state = {
+				items:{},
+				sections: {
+
+				},
+				notes:[]
+		}
 		this.base = Rebase.createClass({
 			apiKey: "AIzaSyB3SNrTzMXAJb6zWhjwD4TDSnITCStq3KU",
 			authDomain: "the-dead-cow.firebaseapp.com",
@@ -17,44 +27,40 @@ export default class FullMenu extends Component {
 			messagingSenderId: "270874696443"
 		}, 'app')
 
-		this.base.fetch('menu', {
-    context: this,
-    asArray: false
-	  }).then(data => {
-			this.setState({
-				menu: data
-			})
-	  }).catch(error => {
-	    //handle error
-	  })
+		this.base.post('/menu', {
+			data: data
+		})
+
+		// this.base.fetch('menu/items', {
+    // context: this,
+    // asArray: true
+	  // }).then(data => {
+		// 	this.setState({
+		// 		items: data
+		// 	})
+	  // }).catch(error => {
+	  //   //handle error
+	  // })
 	}
 
 toggleNav(){
 	Actions.toggleNav();
 }
 
-componentWillMount(){
+componentDidMount(){
 	VarStore.on("change", this.updateNav.bind(this))
 	this.updateNav();
-
-	this.base.bindToState('menu/sections', {
-		context: this,
-		state: 'sections',
-		asArray: false
-	})
 
 	this.base.bindToState('menu/items', {
 		context: this,
 		state: 'items',
-		asArray: false
+		asArray: true
 	})
-
-	this.base.bindToState('menu/notes', {
+	this.base.bindToState('menu/sections', {
 		context: this,
-		state: 'notes',
-		asArray: false
-	});
-
+		state: 'sections',
+		asArray: true
+	})
 }
 
 updateNav(){
@@ -71,20 +77,78 @@ getStyles(){
 	}
 }
 
+renderItem(item){
+	return(
+		<div key={UUID.v4()} className="menu-item">
+			<h4>{item.title}</h4>
+		</div>
+	)
+}
+
+renderItems(section, isSub){
+	if(data.items){
+		let items = data.items
+		let toReturn = []
+		items.map(item => {
+			if(isSub){
+				if(section && item.subCategory && item.subCategory.indexOf(section) >-1){
+					toReturn.push(this.renderItem(item))
+				}
+			}else{
+				if(section && item.category && !item.subCategory && item.category.indexOf(section) >-1){
+					toReturn.push(this.renderItem(item))
+				}
+			}
+
+
+		})
+
+	return toReturn
+	}
+}
+
+renderSubSections(section){
+	let subSec = []
+	if(section.sub)
+	{	section.sub.map(sub => {
+		subSec.push(
+			<div key={UUID.v4()} className="menu-subsection">
+				<h3>{sub.title}</h3>
+				{this.renderItems(sub.slug, true)}
+			</div>
+		)
+	})
+	return subSec;
+	}
+}
+
+renderMenu(){
+	let sec = [];
+	if(data.sections){
+		data.sections.map(section => {
+			sec.push(
+				<div key={UUID.v4()} className="menu-section">
+					<h2>{section.title}</h2>
+					{this.renderItems(section.slug, false)}
+					{this.renderSubSections(section)}
+				</div>
+			)
+		})
+		return sec;
+	}
+}
+
 render() {
 
-	console.log(this.state)
+	console.log(data)
 	return (
-		<div className="cover">
-			<div id="background-image">
-				<div className="wrapper">
-					<div className="container">
-						<div className="front-matter">
-						</div>
-					</div>
+		<div className="full-menu">
+			<div className="wrapper">
+				<div className="container">
+					{this.renderMenu()}
 				</div>
 			</div>
-		</div>
+	</div>
 	)
 }
 }
