38802b2ee9edcbe8c9cb9450c51ecf0ea7dc5ffe
Trevor Sargent <tbird1710@gmail.com>
2017-08-31 12:45:03 -0700

    hide menu

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,24 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export default class NavLinks extends Component {
 
-  render() {
-    return (
-      <div className="navigation">
-        <ul>
-          {/* <li><a href="/specials"><span>Daily </span>Specials</a></li> */}
-          <li>
+	render() {
+		return (
+			<div className="navigation">
+				<ul>
+					{/* <li><a href="/specials"><span>Daily </span>Specials</a></li> */}
+					{/* <li>
             <a href="/menu">Menu</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="#daily-specials">Daily Specials</a>
-          </li>
+          </li> */}
+					<li>
+						<a href="/about">About</a>
+					</li>
+					<li>
+						<a href="#daily-specials">Daily Specials</a>
+					</li>
 
-        </ul>
-      </div>
-    )
-  }
+				</ul>
+			</div>
+		)
+	}
 }
